fb53f7f3b6f386430ac31a24ab1ff40f1204586f
Nicobuzeta <nicolas@buzeta.net>
2023-07-12 15:52:28 -0400

    Parity wayland-config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
+if test -z "$WAYLAND_DISPLAY" ; and test "$XDG_VTNR" != "" ; and test "$XDG_VTNR" -eq 1 ; and test "$SSH_TTY" != "/dev/pts/0"
+    sway-nvidia
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
@@ -5,4 +9,6 @@ end
 
 set fish_greeting ""
 load_nvm
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias config='/usr/bin/git --git-dir=$HOME/wayland_config --work-tree=$HOME'
+
+
